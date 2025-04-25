0c67ba79c37f087599f8f0dfeb687d916968fccf
Dmitrijs Lobanovskis <dlobanovskis@gmail.com>
2025-04-25 11:29:31 +0100

    Visibility interface implementation

--- src/main/java/org/actum/core/If.java
+++ src/main/java/org/actum/core/If.java
@@ -1,5 +1,11 @@
 package org.actum.core;
 
+import org.actum.visibility.Debuggable;
+import org.actum.visibility.Describable;
+import org.actum.visibility.Traceable;
+import org.actum.visibility.Viewable;
+
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 /**
@@ -10,9 +16,12 @@ import java.util.function.Supplier;
  * .elseIf(condition, action)
  * .elseThen(action)
  */
-public class If {
+public class If implements Debuggable, Describable<If>, Traceable<If>, Viewable<If> {
 
     private boolean matched;
+    private String label;
+    private String description;
+    private boolean traceable = false;
 
     /**
      * Accepts condition on what act upon
@@ -76,5 +85,76 @@ public class If {
         if (!this.matched) {
             throw exception.get();
         }
+    }
+
+    /**
+     * Returns debuggable string
+     *
+     * @return debuggable string
+     */
+    @Override
+    public String debug() {
+        return String.format("If[Label=%s, Description=%s, Traceable=%s, Matched=%s]", label, description, traceable, matched);
+    }
+
+    /**
+     * Labels current condition
+     *
+     * @param label label
+     * @return label
+     */
+    @Override
+    public If label(String label) {
+        this.label = label;
+        return this;
+    }
+
+    /**
+     * Adds description to condition
+     *
+     * @param describe description message
+     * @return description
+     */
+    @Override
+    public If describe(String describe) {
+        this.description = describe;
+        return this;
+    }
+
+    /**
+     * Trace current condition
+     *
+     * @return trace
+     */
+    @Override
+    public If trace() {
+        this.traceable = true;
+        return this;
+    }
+
+    /**
+     * Used for side effects or debugging
+     *
+     * @param action action
+     * @return action result
+     */
+    @Override
+    public If peak(Runnable action) {
+        if (this.matched){
+            action.run();
+        }
+        return null;
+    }
+
+    /**
+     * Used for side effects or debugging
+     *
+     * @param consumer consumer
+     * @return consumer result
+     */
+    @Override
+    public If view(Consumer<If> consumer) {
+        consumer.accept(this);
+        return this;
     }
 }
